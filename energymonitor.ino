1d952e0137369988aef357bac33d498b51eec32f
Stoffe @greenLaptop <stoffe@greenlaptop.com>
2014-06-17 08:40:51 +0200

    added a second counter an kwh

--- energymonitor.ino
+++ energymonitor.ino
@@ -15,16 +15,29 @@ IPAddress ip(192, 168, 1, 200); // IP address, may need to change depending on n
 
 char macstr[18];
 
+/*
+* husets mätare sitter på kanal 1 chan1_pin
+*  värmepumpen sitter på kanal 2 chan2_pin  
+*/
+
+
 const int chan1_pin = 1;
-//My meter flashes 1000 times per kWh
-const float w_per_pulse = 1;
+const int chan2_pin = 0; // STOFFE rätt pinne
+            
+const float w_per_pulse = 1;   //My meter flashes 1000 times per kWh
 const unsigned long ms_per_hour = 3600000UL;
 
+const int update_time = 5000; //in ms
+
 unsigned int chan1_count = 0;
+unsigned int chan2_count = 0;
 unsigned long report_time = 0;
 unsigned long chan1_first_pulse = 0;
 unsigned long chan1_last_pulse = 0;
 volatile byte chan1_pulse = 0;
+unsigned long chan2_first_pulse = 0;
+unsigned long chan2_last_pulse = 0;
+volatile byte chan2_pulse = 0;
 
 
 EthernetClient ethClient;
@@ -36,6 +49,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void chan1_isr() {
   chan1_pulse = 1;
 }
+
+void chan2_isr() {
+  chan2_pulse = 1;
+}
+
 
 boolean connect_mqtt() {
   Serial.print("MQTT...");
@@ -52,17 +70,16 @@ boolean connect_mqtt() {
 
 void setup() {
   pinMode(chan1_pin, INPUT);
+  pinMode(chan2_pin, INPUT);
   attachInterrupt(1, chan1_isr, FALLING);
+  attachInterrupt(2, chan1_isr, FALLING); //STOFFE kolla upp vilken pinns
   Serial.begin(9600);
-  delay(1000);µ
+  delay(1000);
   snprintf(macstr, 18, "%02x:%02x:%02x:%02x:%02x:%02x",
     mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   Serial.print(macstr);
   Serial.print(")...");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed.");
-    while(true);
-  }
+  Ethernet.begin(mac,ip);
   Serial.println(Ethernet.localIP());
 }
 
@@ -70,7 +87,11 @@ void loop() {
   char topic[35];
   char value[6];
   static unsigned int chan1_watts;
-  unsigned long chan1_delta; // Time since last pulse
+  static unsigned int chan2_watts;
+  static unsigned int chan1_kwh;
+  static unsigned int chan2_kwh;
+  unsigned long chan1_delta; // Time since last pulse on chan1
+  unsigned long chan2_delta; // Time since last pulse on chan2
 
   if (!client.connected() && !connect_mqtt()) {
     return;
@@ -79,23 +100,50 @@ void loop() {
 
   if (millis() - chan1_last_pulse > 60000) {
     chan1_watts = 0;
+    chan2_watts = 0;
+    chan1_kwh = 0;
+    chan2_kwh = 0;
   }
 
 
   if (millis() - report_time > 5000) {
+
     chan1_delta = chan1_last_pulse - chan1_first_pulse;
     if (chan1_delta && chan1_count) {
       chan1_watts = (chan1_count - 1) * w_per_pulse * ms_per_hour / chan1_delta;
+      chan1_kwh=(chan1_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
       chan1_count = 0;
     }
 
-    if (!(report_time == 0 && chan1_watts == 0)) {
-      snprintf(topic, 35, "house/power/meter/1/current");
+    chan2_delta = chan2_last_pulse - chan2_first_pulse;
+    if (chan2_delta && chan2_count) {
+      chan2_watts = (chan2_count - 1) * w_per_pulse * ms_per_hour / chan2_delta;
+      chan2_kwh=(chan2_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
+      chan2_count = 0;
+    }
+
+    if (!(report_time == 0 && chan1_watts == 0 && chan2_watts == 0)) 
+    {
+      snprintf(topic, 35, "house/power/meter/all/current");
       snprintf(value, 6, "%d", chan1_watts);
       client.publish(topic, value);
+
+      snprintf(topic, 35, "house/power/meter/heat/current");
+      snprintf(value, 6, "%d", chan2_watts);
+      client.publish(topic, value);
+
+      snprintf(topic, 35, "house/power/meter/all/kwh");
+      snprintf(value, 6, "%d", chan2_watts);
+      client.publish(topic, value);
+
+      snprintf(topic, 35, "house/power/meter/heat/kwh");
+      snprintf(value, 6, "%d", chan2_watts);
+      client.publish(topic, value);
+
       Serial.print("Chan1 ");
       Serial.println(chan1_watts);
- 
+      Serial.print("Chan2 ");
+      Serial.println(chan2_watts);
       report_time = millis();
     }
   }
@@ -109,7 +157,19 @@ void loop() {
       chan1_first_pulse = chan1_last_pulse;
     }
 
-    snprintf(topic, 35, "house/power/meter/1/usage");
+    snprintf(topic, 35, "house/power/meter/all/pulse");
+    client.publish(topic, "1");
+  }
+  if (chan2_pulse == 1) {
+    chan2_count++;
+    chan2_pulse = 0;
+
+    chan2_last_pulse = millis();
+    if (chan2_count == 1) { // was reset
+      chan2_first_pulse = chan2_last_pulse;
+    }
+
+    snprintf(topic, 35, "house/power/meter/heat/pulse");
     client.publish(topic, "1");
   }
 }
