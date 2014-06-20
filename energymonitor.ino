461e3679c03bb27e6ab7a16d0d83ea2b2afe44ec
Stoffe @greenLaptop <stoffe@greenlaptop.com>
2014-06-20 07:24:13 +0200

    cleanup

--- energymonitor.ino
+++ energymonitor.ino
@@ -11,7 +11,7 @@
 
 // MAC address from Ethernet shield sticker under board
 byte mac[6] = { 0x90, 0xA2, 0xDA, 0x0E, 0xA8, 0x54 };
-IPAddress ip(192, 168, 1, 200); // IP address, may need to change depending on network
+byte ip[] = { 192, 168, 1, 200 }; //IP addrs
 
 char macstr[18];
 
@@ -25,6 +25,9 @@ const int chan1_pin = 1;
 //const int chan2_pin = 0; // STOFFE rätt pinne
 //My meter flashes 1000 times per kWh            
 const float w_per_pulse = 1;   
+
+todo: number of pulses for chan2 /kwh...is not correct
+
 const unsigned long ms_per_hour = 3600000UL;
 
 const int update_time = 5000; //in ms
@@ -51,9 +54,7 @@ void chan1_isr() {
 }
 
 void chan2_isr() {
-    
   chan2_pulse = 1;
-   Serial.println("IRQ");
 }
 
 
@@ -81,12 +82,9 @@ void setup() {
   snprintf(macstr, 18, "%02x:%02x:%02x:%02x:%02x:%02x",
     mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   Serial.print(macstr);
-  Serial.print(").version:0.4 ..");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed.");
-    while(true);
-  }
-  Serial.println(Ethernet.localIP());
+  Serial.print(").version:0.5 ..");
+  Ethernet.begin(mac,ip);
+//  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
@@ -94,8 +92,6 @@ void loop() {
   char value[6];
   static unsigned int chan1_watts;
   static unsigned int chan2_watts;
-  static unsigned int chan1_kwh;
-  static unsigned int chan2_kwh;
   unsigned long chan1_delta; // Time since last pulse on chan1
   unsigned long chan2_delta; // Time since last pulse on chan2
 
@@ -107,8 +103,6 @@ void loop() {
   if (millis() - chan1_last_pulse > 60000) {
     chan1_watts = 0;
     chan2_watts = 0;
-    chan1_kwh = 0;
-    chan2_kwh = 0;
   }
 
 
@@ -117,14 +111,14 @@ void loop() {
     chan1_delta = chan1_last_pulse - chan1_first_pulse;
     if (chan1_delta && chan1_count) {
       chan1_watts = (chan1_count - 1) * w_per_pulse * ms_per_hour / chan1_delta;
-      chan1_kwh=(chan1_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
+//      chan1_kwh=(chan1_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
       chan1_count = 0;
     }
 
     chan2_delta = chan2_last_pulse - chan2_first_pulse;
     if (chan2_delta && chan2_count) {
       chan2_watts = (chan2_count - 1) * w_per_pulse * ms_per_hour / chan2_delta;
-      chan2_kwh=(chan2_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
+//      chan2_kwh=(chan2_watts*(update_time/1000))/(1000*3600);  /* räknar i sekunder */
       chan2_count = 0;
     }
 
@@ -138,15 +132,7 @@ void loop() {
       snprintf(value, 6, "%d", chan2_watts);
       client.publish(topic, value);
 
-      snprintf(topic, 35, "house/power/meter/all/kwh");
-      snprintf(value, 6, "%d", chan2_watts);
-      client.publish(topic, value);
-
-      snprintf(topic, 35, "house/power/meter/heat/kwh");
-      snprintf(value, 6, "%d", chan2_watts);
-      client.publish(topic, value);
-
-      Serial.print("Chan1 ");
+       Serial.print("Chan1 ");
       Serial.println(chan1_watts);
       Serial.print("Chan2 ");
       Serial.println(chan2_watts);
@@ -155,6 +141,7 @@ void loop() {
   }
 
   if (chan1_pulse == 1) {
+     Serial.println("chan1 puls");
     chan1_count++;
     chan1_pulse = 0;
 
@@ -188,8 +175,4 @@ void loop() {
     3: rebind fail
     4: rebind success
   */
-  if (dhcp_status) {
-    long now = millis();
-    Serial.println("DHCP Lease");
-  }
 }
